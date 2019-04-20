9b2937ed6b4e9af78bd29b5fb948b24ffe13de5f
YGamal1 <46260648+YGamal1@users.noreply.github.com>
2019-04-20 13:52:29 +0200

    Update lotsofmenus.py

--- lotsofmenus.py
+++ lotsofmenus.py
@@ -1,11 +1,9 @@
-#!/usr/bin/env python2
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from database_setup import Categorie, Base, CategorieItem, User
 
-engine = create_engine('postgresql://catalog:catalog/catalog')
+engine = create_engine('postgresql://catalog:catalog@localhost/catalog')
 # Bind the engine to the metadata of the Base class so that the
 # declaratives can be accessed through a DBSession instance
 Base.metadata.bind = engine
